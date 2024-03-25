f1aa68af59eb48cf12fe8cd2c6e175bd7c61b351
Wisney <wisneymaciel2@hotmail.com>
2024-03-25 16:15:10 -0300

    added Category seeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,17 +2,16 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder {
+class CategorySeeder extends Seeder {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run() {
-        $this->call([
-            CategorySeeder::class,
-        ]);
+        Category::factory()->count(20)->create();
     }
 }
